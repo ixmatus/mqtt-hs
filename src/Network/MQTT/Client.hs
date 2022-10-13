ae5d5cdf48257416a14ac10e8f8ea8a89a186859
Parnell Springmeyer <parnell@arista.com>
2022-10-13 18:05:05 -0500

    Don't mask exceptions...

--- src/Network/MQTT/Client.hs
+++ src/Network/MQTT/Client.hs
@@ -312,10 +312,10 @@ runMQTTConduit mkconn MQTTConfig{..} = do
   pure cli
 
   where
-    clientThread cli = E.finally connectAndRun markDisco
+    clientThread cli = connectAndRun
       where
         connectAndRun = mkconn $ \ad -> start cli ad >>= run ad
-        markDisco = atomically $ do
+        _markDisco = atomically $ do
           st <- readTVar (_st cli)
           guard $ st == Starting || st == Connected
           writeTVar (_st cli) Disconnected
